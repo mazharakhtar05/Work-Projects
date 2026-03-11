17214770026e907aa15e9dea9c3d92cfe278a5e5
Mazhar Ansari <mazharakhtar679@gmail.com>
2026-03-12 04:02:11 +0530

    Added Prime Number Check Program

---
+++ JavaPractice/CheckPrime.java
@@ -0,0 +1,37 @@
+// Define a method to find out if a number is prime or not.
+
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+
+    static void checkPrime(int num){
+
+        if(num<=1){
+            System.out.println("Not Prime ");
+            return;
+        }
+       
+        for (int i = 2; i < num; i++) {
+            if(num%i == 0){
+                System.out.println(num + " Not Prime ");
+                return;
+            }
+                
+        }
+
+        
+        System.out.println(num + " Prime ");
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check prime: ");
+        int num = sc.nextInt();
+        checkPrime(num);
+        sc.close();
+    
+    }
+    
+}
